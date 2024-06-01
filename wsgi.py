6ad5bd4893e30b19d9df44b0732edff1c2861175
Bharat Naik <bharat.anaik2003@gmail.com>
2024-06-01 22:24:33 +0530

    BASE TEMPLATE

---
+++ wsgi.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def home():
+    return render_template('home.html')
+
+@app.get('/about')
+def about():
+    return render_template('about.html')
+
+@app.get('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.get('/disclaimer')
+def disclaimer():
+    return render_template('disclaimer.html')
+
